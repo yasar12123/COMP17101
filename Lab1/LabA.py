298042418d0b812405a8212e790d73bfc138080f
yasar12123 <123254904+yasar12123@users.noreply.github.com>
2023-04-25 17:37:34 +0100

    lab a

--- Lab1/LabA.py
+++ Lab1/LabA.py
@@ -4,14 +4,25 @@ import pandas as pd
 df = pd.read_csv("bankruptcy_data_set.csv")
 
 #number of features (e.g. no. of rows, columns)
-print(df.shape)
+#print(df.shape)
 
 #column names and types
-print(df.info())
+#print(df.info())
 
 #remove column Company
 df.drop('Company', axis=1, inplace=True)
 
+#remove all "NaN" rows
+df2 = df.dropna()
+
+#sum WC/TA & RE/TA
+df2['Sum of WC/TA & RE/TA'] = df2['WC/TA'] + df2['RE/TA']
+
+#Multiply EBIT/TA & S/TA
+df2['Product of EBIT/TA & S/TA'] = df2['EBIT/TA'] + df2['S/TA']
+
+
+print(df2)
 
 
 
